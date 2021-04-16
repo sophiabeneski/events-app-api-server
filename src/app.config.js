df815a0480d29c498866eb90e5242e3b1d0b6782
sophiabeneski <sophiabeneski@yahoo.com>
2021-04-16 20:03:33 +0000

    3rd commit

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    version = '1.0.3';
+    version = '1.0.2';
     team = 'The Cloud Crew';
 }
 module.exports = AppConfig;
